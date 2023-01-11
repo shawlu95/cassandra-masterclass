50473fb2d4e2925c21a9d25980b42bc9c66d2974
Shaw Lu <shawlu95@gmail.com>
2023-01-10 17:44:01 -0800

    accept vehicle id and date params from get request

--- java-client/src/main/java/com/vehicletracker/servlet/VehicleTrackerServlet.java
+++ java-client/src/main/java/com/vehicletracker/servlet/VehicleTrackerServlet.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.InetSocketAddress;
+import java.util.List;
 
 @WebServlet("/track")
 public class VehicleTrackerServlet extends HttpServlet {
@@ -32,24 +33,55 @@ public class VehicleTrackerServlet extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html");
 
-        String vehicleId = "CA6AFL218";
-        String trackDate = "2014-05-19";
+        String vehicleId = request.getParameter("veh_id");
+        String trackDate = request.getParameter("date_val");
         String cql = "select time, latitude, longitude from location where vehicle_id = '"
                 + vehicleId + "' and date = '" + trackDate + "';";
         SimpleStatement statement = SimpleStatement.newInstance(cql);
         ResultSet result = session.execute(statement);
+        List<Row> rows = result.all();
+
+        System.out.println(cql);
+        System.out.println(String.format("Found %d records", rows.size()));
 
         PrintWriter out = response.getWriter();
-        out.println("<html><head></head><body>");
+        out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\"><html>");
+        out.println("<head><title>Track a Vehicle</title></head>");
+        out.println("<body>");
+        out.println("<h1>Track a Vehicle</h1>");
+        out.println("Enter the track date and id of the vehicle you want to track");
+        out.println("<p>&nbsp;</p>");
+        out.println("<form id=\"form1\" name=\"form1\" method=\"get\" action=\"\">");
         out.println("<table>");
-        for (Row row : result) {
-            out.println("<tr>");
-            out.println("<td>" + row.getInstant("time") + "</td>");
-            out.println("<td>" + row.getDouble("latitude") + "</td>");
-            out.println("<td>" + row.getDouble("longitude") + "</td>");
-            out.println("</tr>");
-        }
+        out.println("<tr><td>Date (e.g. 2014-05-19):</td>");
+        out.println("<td><input type=\"text\" name=\"date_val\" id=\"date_val\"/></td></tr>");
+        out.println("<tr><td>Vehicle id (e.g. FLN78197):</td>");
+        out.println("<td><input type=\"text\" name=\"veh_id\" id=\"veh_id\" /></td></tr>");
+        out.println("<tr><td></td><td><input type=\"submit\" name=\"submit\" id=\"submit\" value=\"Submit\"/></td></tr>");
         out.println("</table>");
+        out.println("</form>");
+        out.println("<p>&nbsp;</p>");
+
+        if (request.getParameter("veh_id") == null) {
+            // blank
+        } else if (rows.size() == 0) {
+            out.println("<hr/>");
+            out.println("<p>&nbsp;</p>");
+            out.println("Sorry, no results for vehicle id " + request.getParameter("veh_id") + " for " + request.getParameter("date_val"));
+        } else {
+            out.println("<hr/>");
+            out.println("<table cellpadding=\"4\">");
+            out.println("<tr><td colspan=\"3\"><h2>" + request.getParameter("veh_id") + "</h2></td></tr>");
+            out.println("<tr><td><b>Date and Time</b></td><td><b>Latitude</b></td><td><b>Longitude</b></td></tr>");
+            for (Row row : rows) {
+                out.println("<tr>");
+                out.println("<td>" + row.getInstant("time") + "</td>");
+                out.println("<td>" + row.getDouble("latitude") + "</td>");
+                out.println("<td>" + row.getDouble("longitude") + "</td>");
+                out.println("</tr>");
+            }
+            out.println("</table>");
+        }
         out.println("</body></html>");
     }
 }
