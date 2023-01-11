e24c0f638a5530f5b8a49695bd995a0bfe36e125
Shaw Lu <shawlu95@gmail.com>
2023-01-10 16:39:56 -0800

    init java servlet

---
+++ java-client/src/main/java/com/vehicletracker/servlet/VehicleTrackerServlet.java
@@ -0,0 +1,24 @@
+package com.vehicletracker.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/track")
+public class VehicleTrackerServlet extends HttpServlet {
+    private static final  long serialVersionUID = 1L;
+
+    public VehicleTrackerServlet () {
+        super();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h3>Hello World!</h3>");
+    }
+}
